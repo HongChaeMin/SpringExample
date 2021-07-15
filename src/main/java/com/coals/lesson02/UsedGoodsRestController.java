56bbbfb7a0d756ccf15438987c9732157fe5c4e6
DESKTOP-CQH0EH6\ghdco <ghdcoalss33@gmail.com>
2021-07-15 17:51:56 +0900

    수정

--- src/main/java/com/coals/lesson02/UsedGoodsRestController.java
+++ src/main/java/com/coals/lesson02/UsedGoodsRestController.java
@@ -15,7 +15,7 @@ public class UsedGoodsRestController {
 	@Autowired
 	private UsedGoodsBO usedGoodsBO;
 	
-	@RequestMapping("lesson02/ex01")
+	@RequestMapping("/lesson02/ex01")
 	public List<UsedGoods> ex01() {
 		List<UsedGoods> list = usedGoodsBO.getUsedGoodsList();
 		return list; // jackson -> json
